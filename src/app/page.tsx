80c721ea7878e010a8b895d948a068bae6619937
ERuvinga <ruvingaelie@gmail.com>
2025-03-24 17:12:32 +0200

    feat: Setup Jeannine Banza Project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// Home Page
+const Home = () => {
+  return (
+    <div className='w-[100%] h-[100svh] bg-Brand-neutral-800 flex flex-row'>
+      Zawaa Application
+    </div>
+  );
+};
+export default Home;
